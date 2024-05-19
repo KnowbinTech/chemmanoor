d2fb48709804a3b4f33050be247b4e96e34df65a
renoycb <48327628+renoycb@users.noreply.github.com>
2024-05-19 13:13:37 +0530

    login route

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+</script>
+
+<div class="flex items-center justify-center h-screen">
+    <h1 class="text-3xl font-bold text-center">Login will be accessible to members soon</h1>
+</div>
